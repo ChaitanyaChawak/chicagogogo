5c22b3ee97b59e948f7a5583c756ffb50ad7a791
Chaitanya <chaitanyachawak001@gmail.com>
2023-07-11 02:35:06 +0530

    added exceptions

--- main.py
+++ main.py
@@ -12,9 +12,25 @@ class Chicagoify:
         self.question = input("Type \'blocks\' if you want to get the number of blocks from the center of Chicago, else type \'address\' to get the address given the number of blocks : ")
         if self.question == 'address':
             self.north = input('Number of blocks away to north : ')
+            try:
+                float(self.north)
+            except ValueError:
+                raise Exception("Please enter a valid positive number !!")
             self.south = input('Number of blocks away to south : ')
+            try:
+                float(self.south)
+            except ValueError:
+                raise Exception("Please enter a valid positive number !!")
             self.east = input('Number of blocks away to east : ')
+            try:
+                float(self.east)
+            except ValueError:
+                raise Exception("Please enter a valid positive number !!")
             self.west = input('Number of blocks away to west : ')
+            try:
+                float(self.west)
+            except ValueError:
+                raise Exception("Please enter a valid positive number !!")
 
             if self.north and self.south != '0' :
                 raise Exception("The place cannot be both North and South at the same time !")
